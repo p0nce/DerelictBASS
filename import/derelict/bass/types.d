c1bb456ece66759c6e30ab1b09f3e73c083508cc
ponce <aliloko@gmail.com>
2013-09-03 22:16:20 +0200

    add dub package.json

--- import/derelict/bass/types.d
+++ import/derelict/bass/types.d
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2008 Derelict Developers
+ * Copyright (c) 2004-2013 Derelict Developers
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
